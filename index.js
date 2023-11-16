35e5c2be2225bf8012264d6851c64b8731bb6920
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-16 12:28:21 -0300

    avance

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-import app from './src/app.js'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import app from "./src/app.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import libroServicers from "./src/services/libro-services";
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
 
-const __filename = fileURLToPath(import.meta.url)
-export const __dirname = dirname(__filename)
-
-console.clear()
+console.clear();
 const port = 3001;
-
-app.listen(port, ()=>{console.log(`The port is ${port}`);});
+const libro = new libroServicers();
+app.post("/libros", async (req, res) => {
+  try {
+    res.json(await libro.Getall());
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json("error en el servidor");
+  }
+});
+app.listen(port, () => {
+  console.log("ESCUCHANDO PORT 5000");
+});
